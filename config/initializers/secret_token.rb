501418a7b8057437bbe12c4f86939876c83f4cac
Madzia <madzia@madzia-System-Product-Name.(none)>
2013-01-23 22:13:38 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LatexTutorial::Application.config.secret_token = '5bc4ff574b34b4c7212988bfb848385b5f9323eff410db271e7a7e1d7c9589c2a21cfc5906f66f7e2c7e7599e92f6e410ed79800017192611b8b462c31364549'
